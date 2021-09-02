8b595e9ce0b6fb0f46f3580548586b86743c3df2
aldycavalera <personal.cavalera@gmail.com>
2021-09-02 11:29:58 +0700

    feat: add jit mode for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ delete colors?.lightBlue
 
 module.exports = {
   purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class' mode:'jit',
+  darkMode: false,
+  mode: 'jit',
   plugins: [require('tailwind-filter-utilities')]
 }
